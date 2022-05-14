8f6f84185e5e100cf3cdb1a0d7895b3e22b5b89b
Sid <sidharthraj1995@gmail.com>
2022-05-13 22:10:10 -0400

    defined Rotate String Function

--- rotateString.cpp
+++ rotateString.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
+/* 
+Rotate each char of the string and return the rotated string
+ */
+string rotateString(string c) {
+    string rotStr;
+    for(int i=0; i <c.length(); i++) {
+        if (c[i] == 'z' || c[i] == 'Z')
+        {
+            rotStr.push_back(c[i]-25);
+        } else
+        rotStr.push_back(c[i]+1);
+    }    
+    return rotStr;
+}
+
+
 int main() {
-    char c;
-    cout << "Enter a character: ";
-    cin >> c;
-    cout << "ASCII Value of " << c << " is " << int(c);
+
+    string test;
+    // cout << "Input a string to rotate: ";
+    cin >> test;
+    // cout << "This is the input: " << test << endl;
+
+
+    // string f = rotateString(test);
+    cout << rotateString(test);
+
     return 0;
 }
