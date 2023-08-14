fb9a9e9bd1f200cdd2d16a753bb1126f64147b92
Sid <sidharthraj1995@gmail.com>
2023-08-13 21:22:20 -0500

    Birthday Card

--- AndroidStudio/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ AndroidStudio/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.happybirthday
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,13 +38,21 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
-                        Column(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
+                        Column {
                             Welcome(
-                                size = 30f,
-                                color = Color.Yellow
+                                size = 30,
+                                color = Color.White
+                            )
+                            GreetingImage(
+                                message = "Alex",
+                                from = "Sid",
+                                size = 60,
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .fillMaxSize()
+                                    .align(alignment = Alignment.CenterHorizontally)
                             )
                         }
-                        GreetingImage(message = "Alex", from = "Sid")
 //                        GreetingText(name = "Alex", from = "Sid", type = Events.NA, size = 20f)
 //                        GreetingText(name = "Alex", from = "Sid", type = Events.WC, size = 20f)
 //                        GreetingText(name = "Alex", from = "Sid", type = Events.HA, size = 30f)
@@ -55,17 +65,31 @@ class MainActivity : ComponentActivity() {
 }
 
 
-val readOnlyImageMenu = mapOf<String, String>("card1" to "R.drawable.androidparty")
+val readOnlyImageMenu = mapOf<String, Int>("card1" to R.drawable.androidparty)
 
 @Composable
-fun GreetingImage(message: String, from : String, modifier: Modifier = Modifier) {
+fun GreetingImage(
+    message: String,
+    from: String,
+    size: Int,
+    modifier: Modifier = Modifier
+) {
     val image = painterResource(R.drawable.androidparty)
     Box {
         Image(
             painter = image,
-            contentDescription = "Birthday"
+            contentDescription = "Birthday",
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
         )
-        GreetingText(name = message, from = from, type = Events.HA, size = 30f)
+        GreetingText(
+            name = message,
+            from = from,
+            type = Events.HA,
+            size = size,
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+        )
     }
 }
 
@@ -74,7 +98,7 @@ fun GreetingText(
     name: String,
     from: String,
     type: Events,
-    size: Float,
+    size: Int,
     modifier: Modifier = Modifier
 ) {
     /*
@@ -87,38 +111,44 @@ fun GreetingText(
         Events.WC -> "Welcome, "
         Events.NA -> "Namaste, "
     }
-    Column {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier,
+    ) {
         Text(
             text = "$greet$name!",
             modifier = Modifier.padding(8.dp),
             fontSize = size.sp,
-            lineHeight = size.sp * 1.1f,
+            lineHeight = size.sp * 1.1,
+            textAlign = TextAlign.Center
         )
         Text(
             text = "From $from",
             modifier = Modifier
-                //            .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
                 .padding(4.dp),
-            fontSize = size.sp / 2f
+            fontSize = size.sp / 2
         )
     }
 }
 
 @Composable
 fun Welcome(
-    size: Float,
+    size: Int,
     color: Color,
     modifier: Modifier = Modifier
 ) {
     Text(
         text = "Welcome to Android Studio Course!!",
         modifier = Modifier
-            .background(color = Color.Cyan)
+            .background(color = Color.Black)
+//            .align(alignment = Alignment.CenterHorizontally)
+//            .alpha(0.5f)
             .padding(10.dp),
         fontSize = size.sp,
-        lineHeight = size.sp*1.2f,
+        lineHeight = size.sp*1.2,
         color = color,
-        textAlign = TextAlign.Center
+        textAlign = TextAlign.Center,
     )
 }
 
@@ -132,7 +162,7 @@ fun Welcome(
 fun GreetingPreview() {
     HappyBirthdayTheme {
 //        GreetingText(message = "Happy Birthday, Sam!")
-        GreetingImage(message = "Alex", from = "Sid")
+//        GreetingImage(message = "Alex", from = "Sid")
     }
 }
 
