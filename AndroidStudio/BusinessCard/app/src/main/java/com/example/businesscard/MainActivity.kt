87e39fe3863226e651e2739e3ff179c90313ae53
Sid <sidharthraj1995@gmail.com>
2023-08-14 21:41:38 -0500

    BusinessCard
    Android Studio Developer Course > Business Card

---
+++ AndroidStudio/BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BusinessCard() {
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BusinessCardTheme {
+        Greeting("Android")
+    }
+}
+
+class Person(
+    name: String,
+    title: String,
+    phone: Int,
+    social: String,
+    email: String
+) {
+    val name = name.replaceFirstChar { it.uppercase() }
+    val title = title.replaceFirstChar { it.uppercase() }
+    val phone = phone
+    val social = "@$social"
+    val email = email
+}
